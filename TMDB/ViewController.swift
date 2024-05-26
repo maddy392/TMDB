da2ad67c77fae48cb3da52e1f25be6e1fe8759ca
maddy392 <maddy392@gmail.com>
2024-05-25 20:31:15 -0500

    Initial Commit

---
+++ TMDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMDB
+//
+//  Created by Madhu Babu Adiki on 5/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
